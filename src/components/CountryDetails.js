5c9a4e7eaddd53d8bf2946f12847ec61f184df4c
ialmeida <ialmeida.pb@gmail.com>
2021-04-19 17:30:37 -0300

    done

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -12,24 +12,38 @@ class CountryDetails extends React.Component{
 
       };
 
-      componentDidMount() {
+      componentDidUpdate(prevProps, prevState) {
         const country = countries.find((country) => {
           return country.cca3 === this.props.match.params.countryCode; // match é um objeto injetado pelo componente Route automaticamente. Ele contém informações sobre essa rota, inclusive os parâmetros de rota. A propriedade se chama 'id' pois esse nome que escolhemos para o parâmetro de rota no App.js. O valor dela é a porção variável da URL
         });
     
 
     
-        if (country) {
+         if (prevProps.match.params.countryCode !== this.props.match.params.countryCode) {
           this.setState({
             name: country.name.official,
             capital: country.capital,
-            borders: country.borders,
+            borders: [...country.borders],
             area: country.area,
 
           });
         }
       }
 
+
+renderBorderName=(countryCode)=>{
+const foundCountry = countries.find((country)=>{
+  return country.cca3 === countryCode;
+});
+
+if (foundCountry) {
+  return foundCountry.name.common;
+} else {
+  return 'Country Not Found';
+}
+
+
+}
 
 render=()=>{
 return(
@@ -38,13 +52,15 @@ return(
 <hr/>
 <h3>Capital:</h3><p>{this.state.capital}</p>
 <hr/>
-<h3>Borders:</h3><ul>
-                        {this.state.borders.map((border) => {
-                            return <li>
-                                {border}
-                            </li>
-                        })}
-                    </ul>
+<h3>Borders:</h3>
+                <ul>
+                {this.state.borders.map((border) => {
+                return( <li>
+                <Link to={border}>{this.renderBorderName(border)}</Link>
+                </li>
+                )
+                })}
+                </ul>
 
 <hr/>
 
